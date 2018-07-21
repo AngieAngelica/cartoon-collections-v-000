69fa2f69b24ff14e6bc86d0a4e494d02b43ddf3e
Angie Kassab <anasab12@gmail.com>
2018-07-21 23:14:18 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,12 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  names.each_with_index do |names, index|
+    puts "#{index + 1} #{names}"
+  end
+  
+
+  
+  # code an argument here
+  
 end
 
 def summon_captain_planet# code an argument here
